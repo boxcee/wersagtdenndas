56aa66d0f4c0aefb96ce32bd77c658cbdcc49876
Moritz Schmitz von Hülst <msc@marketlogicsoftware.com>
2019-10-25 16:54:50 +0200

    more steps

--- components/quote.js
+++ components/quote.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
-const Quote = ({ content, author, source, id }) => <div key={id}>"{content}" said {author} according to {source}</div>;
+const Quote = ({ content, author, source, id }) => (
+  <div>
+    <p>"{content}"</p>
+    <div className='right'>- <a href={source}>{author}</a></div>
+    <style jsx>{`
+      .right {
+        text-align: right;
+      }
+    `}</style>
+  </div>
+);
 
 export default Quote;
